67e31be44d9c59577ece3e51ee8b0fc616b2cc5f
Sujal baidhya <157810505+SujalBaidhya@users.noreply.github.com>
2025-06-01 15:43:54 +0545

    exchandling

--- exc.java
+++ exc.java
@@ -1,13 +1,22 @@
 class exc
 {
      public static void main(String[] args) {
+
+        // try{
+        //     int a=x/0;
+        // }
+        // catch(ArithmeticException e)
+        // {
+        //     System.out.println(e.getMessage());
+        //     e.printStackTrace();
+        // }
+        int arr[]={1,2,3};
         try{
-            int a=10/0;
+            arr[4]=5;
         }
-        catch(ArithmeticException e)
+        catch(ArrayIndexOutOfBoundsException e)
         {
             System.out.println(e.getMessage());
-            e.printStackTrace();
         }
         
     }
