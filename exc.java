7041d36c7b25b117fc822cc07e458224f6c8feb4
Sujal baidhya <157810505+SujalBaidhya@users.noreply.github.com>
2025-06-01 15:35:13 +0545

    exchandle

---
+++ exc.java
@@ -0,0 +1,14 @@
+class exc
+{
+     public static void main(String[] args) {
+        try{
+            int a=10/0;
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        
+    }
+}
